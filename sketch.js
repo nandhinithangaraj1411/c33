6d5db6feb4bbca6528260ddcc0deb2ea82afba05
nandhinithangaraj1411 <nandhinit1411@gmail.com>
2020-05-02 11:40:34 +0530

    api calls

--- sketch.js
+++ sketch.js
@@ -93,11 +93,12 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
+    if(keyCode === 32 && bird.body.speed<1){
+        bird.trajectory=[]
+        Matter.Body.setPosition(bird.body,{x:200,y:50})
+        slingshot.attach(bird.body);
     }
 }
-
 async function getBackgroundImg(){
     var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
     var responseJSON = await response.json();
